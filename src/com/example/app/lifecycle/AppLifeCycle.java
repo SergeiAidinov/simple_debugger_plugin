02d175b866caeaec889d3aa8978783f1c78b9030
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-11-11 21:58:04 +0400

    plugin starts

--- src/com/example/app/lifecycle/AppLifeCycle.java
+++ src/com/example/app/lifecycle/AppLifeCycle.java
@@ -4,13 +4,16 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.ui.IStartup;
+
+import jakarta.annotation.PreDestroy;
 
 
 /**
  * Класс-менеджер жизненного цикла.
  * Методы будут вызываться автоматически системой e4.
  */
-public class AppLifeCycle {
+public class AppLifeCycle implements IStartup{
 
     /**
      * Вызывается на самом раннем этапе, когда создан контекст приложения (аналог start() Activator'а).
@@ -31,10 +34,15 @@ public class AppLifeCycle {
     /**
      * Вызывается, когда приложение закрывается.
      */
-	/*
-	 * @PreDestroy void handlePreDestroy(IEclipseContext context) { String name =
-	 * context.get("appName", "Unknown App");
-	 * System.out.println("❌ LifeCycle Manager: @PreDestroy - Приложение '" + name +
-	 * "' закрывается. Очистка..."); }
-	 */
+	
+	  @PreDestroy void handlePreDestroy(IEclipseContext context) { 
+		  String name = (String) context.get("appName");
+	  System.out.println("❌ LifeCycle Manager: @PreDestroy - Приложение '" + name +
+	  "' закрывается. Очистка..."); }
+
+	  @Override
+		public void earlyStartup() {
+			 System.out.println("AppStartup сработал!");
+		}
+	 
 }
