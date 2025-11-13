ef4340480998b233b6db8405b48422bdf6fc328b
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-11-14 02:22:31 +0400

    cleaned code

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/OnWorkflowReadyListener.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/OnWorkflowReadyListener.java
@@ -1,5 +1,6 @@
 package com.gmail.aydinov.sergey.simple_debugger_plugin.core;
 
+@FunctionalInterface
 public interface OnWorkflowReadyListener {
 	 void onReady(SimpleDebuggerWorkFlow workflow);
 }
