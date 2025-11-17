322523bf35d48df95bf316c3340942445fc84db5
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-11-17 07:30:02 +0400

    changed BreakpointWrapper

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/dto/BreakpointWrapper.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/dto/BreakpointWrapper.java
@@ -3,6 +3,8 @@ package com.gmail.aydinov.sergey.simple_debugger_plugin.dto;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.debug.core.model.IBreakpoint;
 
+import java.util.Optional;
+
 public class BreakpointWrapper implements Comparable<BreakpointWrapper> {
 
     private final IBreakpoint bp;
@@ -11,31 +13,45 @@ public class BreakpointWrapper implements Comparable<BreakpointWrapper> {
 
     public BreakpointWrapper(IBreakpoint bp) {
         this.bp = bp;
-        this.path = bp.getMarker().getResource().getFullPath().toString();
-        this.line = bp.getMarker().getAttribute(IMarker.LINE_NUMBER, -1);
+        this.path = Optional.ofNullable(bp)
+                .map(IBreakpoint::getMarker)
+                .map(IMarker::getResource)
+                .map(r -> r.getFullPath().toString())
+                .orElse("");
+        this.line = Optional.ofNullable(bp)
+                .map(IBreakpoint::getMarker)
+                .map(marker -> marker.getAttribute(IMarker.LINE_NUMBER, -1))
+                .orElse(-1);
     }
 
-    public IBreakpoint get() {
-        return bp;
+    public Optional<IBreakpoint> get() {
+        return Optional.ofNullable(bp);
     }
 
     @Override
     public int compareTo(BreakpointWrapper o) {
-        int c = path.compareTo(o.path);
-        if (c != 0) return c;
-        return Integer.compare(line, o.line);
+        if (o == null) return 1;
+        int cmp = path.compareTo(o.path);
+        return cmp != 0 ? cmp : Integer.compare(line, o.line);
     }
 
     @Override
     public boolean equals(Object obj) {
+        if (this == obj) return true;
         if (!(obj instanceof BreakpointWrapper)) return false;
-        BreakpointWrapper o = (BreakpointWrapper) obj;
-        return path.equals(o.path) && line == o.line;
+        BreakpointWrapper other = (BreakpointWrapper) obj;
+        return line == other.line && path.equals(other.path);
     }
 
     @Override
     public int hashCode() {
-        return 31 * path.hashCode() + line;
+        int result = path.hashCode();
+        result = 31 * result + line;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Breakpoint at " + path + ":" + line;
     }
 }
-
