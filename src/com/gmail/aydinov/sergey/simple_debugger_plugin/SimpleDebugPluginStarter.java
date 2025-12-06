0294562c5459f3f9355870cfe942c282da409a6e
Sergei Aidinov <sergey.aydinov@gmail.com>
2025-12-06 22:08:54 +0400

    starts target

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/SimpleDebugPluginStarter.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/SimpleDebugPluginStarter.java
@@ -3,21 +3,70 @@ package com.gmail.aydinov.sergey.simple_debugger_plugin;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-
-import com.gmail.aydinov.sergey.simple_debugger_plugin.wrapper.TargetAppWrapper;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class SimpleDebugPluginStarter extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
+        Shell shell = HandlerUtil.getActiveShell(event);
+
         try {
-            System.out.println("Launching TargetAppWrapper from plugin...");
+            // 1. Получаем все Java конфигурации таргета
+            ILaunchConfiguration[] configs = DebugPlugin.getDefault()
+                    .getLaunchManager()
+                    .getLaunchConfigurations();
 
-            // Здесь можно передать имя таргет-класса как аргумент
-            String targetClass = "target_debug.Main";
-            TargetAppWrapper.main(new String[]{targetClass});
+            // 2. Диалог для выбора таргет-приложения
+            ElementListSelectionDialog dialog = new ElementListSelectionDialog(shell, new LabelProvider() {
+                @Override
+                public String getText(Object element) {
+                    if (element instanceof ILaunchConfiguration config) {
+                        return config.getName();
+                    }
+                    return super.getText(element);
+                }
+            });
 
-            System.out.println("TargetAppWrapper finished execution.");
+            dialog.setTitle("Select Target App");
+            dialog.setMessage("Choose target application to debug:");
+            dialog.setElements(configs);
+            dialog.setMultipleSelection(false);
+
+            if (dialog.open() != Window.OK) return null;
+            ILaunchConfiguration selectedConfig = (ILaunchConfiguration) dialog.getFirstResult();
+
+            // 3. Задаем параметры для wrapper
+            String targetClass = "target_debug.Main"; // имя таргет-класса
+            String targetBin = "/home/sergei/eclipse-commiters-workspace/target_debug/bin"; // путь к bin
+
+            // 4. Формируем команду для запуска TargetAppWrapper через ProcessBuilder
+            String wrapperClass = "com.gmail.aydinov.sergey.simple_debugger_plugin.wrapper.TargetAppWrapper";
+            String pluginBin = "/home/sergei/eclipse-commiters-workspace/simple_debugger_plugin/bin"; // путь к вашему плагину
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-cp", pluginBin,
+                    "-Dtarget.classpath=" + targetBin,
+                    wrapperClass,
+                    targetClass
+            );
+
+            pb.inheritIO(); // вывод таргет-приложения в консоль wrapper
+
+            System.out.println("Launching wrapper for target: " + targetClass);
+            Process process = pb.start();
+            System.out.println("Wrapper started. Target: " + targetClass);
+
+            // Ждем завершения таргет-приложения
+            int exitCode = process.waitFor();
+            System.out.println("Wrapper finished. Exit code: " + exitCode);
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/wrapper/TargetAppWrapper.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/wrapper/TargetAppWrapper.java
@@ -1,13 +1,43 @@
 package com.gmail.aydinov.sergey.simple_debugger_plugin.wrapper;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class TargetAppWrapper {
 
     public static void main(String[] args) {
-        System.out.println("TargetAppWrapper started!");
-        if (args != null && args.length > 0) {
-            System.out.println("Received target class: " + args[0]);
-        } else {
-            System.out.println("No target class provided.");
+        if (args.length == 0) {
+            System.err.println("Target class name not provided!");
+            return;
+        }
+
+        String targetClassName = args[0];
+        String targetBin = System.getProperty("target.classpath");
+
+        if (targetBin == null) {
+            System.err.println("System property 'target.classpath' not set!");
+            return;
+        }
+
+        System.out.println("Launching target class: " + targetClassName);
+        System.out.println("Target classpath: " + targetBin);
+
+        ProcessBuilder pb = new ProcessBuilder(
+                "java",
+                "-cp", targetBin,
+                targetClassName
+        );
+
+        pb.inheritIO(); // вывод консоли таргет-приложения в консоль wrapper
+
+        try {
+            Process process = pb.start();
+            System.out.println("Target process started.");
+            int exitCode = process.waitFor();
+            System.out.println("Target process exited with code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
