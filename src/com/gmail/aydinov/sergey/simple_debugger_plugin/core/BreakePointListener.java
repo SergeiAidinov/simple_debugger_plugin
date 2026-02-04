0ea272ea52933620746cc49a56c369497b0e45b5
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-02-05 01:07:38 +0400

    core refactoring finished

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/BreakePointListener.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/BreakePointListener.java
@@ -13,92 +13,85 @@ import com.gmail.aydinov.sergey.simple_debugger_plugin.core.interfaces.Breakpoin
 import com.gmail.aydinov.sergey.simple_debugger_plugin.logging.SimpleDebuggerLogger;
 
 /**
- * Listener for Eclipse breakpoints, forwarding events to the registered subscriber.
+ * Listener for Eclipse breakpoints, forwarding events to the registered
+ * subscriber.
  */
 public class BreakePointListener implements IBreakpointListener, BreakpointSubscriberRegistrar {
 
-    private BreakpointSubscriber breakpointSubscriber;
+	private BreakpointSubscriber breakpointSubscriber;
 
-    /**
-     * Called when a breakpoint is added in Eclipse.
-     *
-     * @param breakpoint the breakpoint that was added
-     */
-    @Override
-    public void breakpointAdded(IBreakpoint breakpoint) {
-        try {
-            if (!breakpoint.isEnabled()) {
-                breakpoint.setEnabled(true);
-            }
-        } catch (CoreException exception) {
-            exception.printStackTrace();
-        }
+	/**
+	 * Called when a breakpoint is added in Eclipse.
+	 *
+	 * @param breakpoint the breakpoint that was added
+	 */
+	@Override
+	public void breakpointAdded(IBreakpoint breakpoint) {
+		try {
+			if (!breakpoint.isEnabled()) {
+				breakpoint.setEnabled(true);
+			}
+		} catch (CoreException exception) {
+			exception.printStackTrace();
+		}
+		breakpointSubscriber.addBreakepoint(breakpoint);
+		logBreakpoint(breakpoint, "ADDED");
+	}
 
-        printBreakpoint(breakpoint, "ADDED");
-        breakpointSubscriber.addBreakepoint(breakpoint);
-    }
+	/**
+	 * Called when a breakpoint is removed in Eclipse.
+	 *
+	 * @param breakpoint the breakpoint that was removed
+	 * @param delta      marker delta (ignored)
+	 */
+	@Override
+	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+		breakpointSubscriber.deleteBreakepoint(breakpoint);
+		logBreakpoint(breakpoint, "REMOVED");
+	}
 
-    /**
-     * Called when a breakpoint is removed in Eclipse.
-     *
-     * @param breakpoint the breakpoint that was removed
-     * @param delta marker delta (ignored)
-     */
-    @Override
-    public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
-        printBreakpoint(breakpoint, "REMOVED");
-        breakpointSubscriber.deleteBreakepoint(breakpoint);
-    }
+	/**
+	 * Called when a breakpoint is changed. Currently no action is taken.
+	 *
+	 * @param breakpoint the breakpoint that changed
+	 * @param delta      marker delta (ignored)
+	 */
+	@Override
+	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
+		// No action needed for now
+	}
 
-    /**
-     * Called when a breakpoint is changed.
-     * Currently no action is taken.
-     *
-     * @param breakpoint the breakpoint that changed
-     * @param delta marker delta (ignored)
-     */
-    @Override
-    public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
-        // No action needed for now
-    }
+	/**
+	 * Logs information about a breakpoint.
+	 *
+	 * @param breakpoint the breakpoint to log
+	 * @param action     the action performed (e.g., "ADDED" or "REMOVED")
+	 */
+	private void logBreakpoint(IBreakpoint breakpoint, String action) {
+		try {
+			IResource resource = breakpoint.getMarker().getResource();
+			String projectName = (Objects.nonNull(resource) && Objects.nonNull(resource.getProject()))
+					? resource.getProject().getName()
+					: "Unknown Project";
+			String fileName = (Objects.nonNull(resource)) ? resource.getName() : "Unknown File";
+			int lineNumber = breakpoint.getMarker().getAttribute("lineNumber", -1);
+			boolean enabled = breakpoint.isEnabled();
 
-    /**
-     * Logs information about a breakpoint.
-     *
-     * @param breakpoint the breakpoint to log
-     * @param action the action performed (e.g., "ADDED" or "REMOVED")
-     */
-    private void printBreakpoint(IBreakpoint breakpoint, String action) {
-        try {
-            IResource resource = breakpoint.getMarker().getResource();
-            String projectName = (Objects.nonNull(resource) && Objects.nonNull(resource.getProject()))
-                    ? resource.getProject().getName()
-                    : "Unknown Project";
-            String fileName = (Objects.nonNull(resource))
-                    ? resource.getName()
-                    : "Unknown File";
-            int lineNumber = breakpoint.getMarker().getAttribute("lineNumber", -1);
-            boolean enabled = breakpoint.isEnabled();
+			SimpleDebuggerLogger.info(action + " -> project: " + projectName + ", file: " + fileName + ", line: "
+					+ lineNumber + ", enabled: " + enabled);
 
-            SimpleDebuggerLogger.info(
-                    action + " -> project: " + projectName +
-                            ", file: " + fileName +
-                            ", line: " + lineNumber +
-                            ", enabled: " + enabled
-            );
+		} catch (Exception exception) {
+			SimpleDebuggerLogger.error(exception.getMessage(), exception);
+		}
+	}
 
-        } catch (Exception exception) {
-            SimpleDebuggerLogger.error(exception.getMessage(), exception);
-        }
-    }
-
-    /**
-     * Registers a subscriber that will receive breakpoint events.
-     *
-     * @param breakpointSubscriber the subscriber to register
-     */
-    @Override
-    public void register(BreakpointSubscriber breakpointSubscriber) {
-        this.breakpointSubscriber = breakpointSubscriber;
-    }
+	/**
+	 * Registers a subscriber that will receive breakpoint events.
+	 *
+	 * @param breakpointSubscriber the subscriber to register
+	 */
+	@Override
+	public void register(BreakpointSubscriber breakpointSubscriber) {
+		this.breakpointSubscriber = breakpointSubscriber;
+	}
 }
