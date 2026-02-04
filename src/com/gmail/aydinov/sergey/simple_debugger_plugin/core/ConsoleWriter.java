8f66794ab02c20508c599b7e4beed9d2a0b231f9
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-02-05 01:04:51 +0400

    refactored ConsoleWriter

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/ConsoleWriter.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/ConsoleWriter.java
@@ -11,45 +11,43 @@ import com.gmail.aydinov.sergey.simple_debugger_plugin.event.debug_event.Console
 import com.gmail.aydinov.sergey.simple_debugger_plugin.processor.SimpleDebuggerEventQueue;
 
 /**
- * Reads an InputStream (e.g., process output) and sends each line to the debug console.
+ * Reads an InputStream (e.g., process output) and sends each line to the debug
+ * console.
  */
 public class ConsoleWriter implements Runnable {
 
-    private final InputStream inputStream;
-    private final String prefix;
+	private final InputStream inputStream;
+	private final String prefix;
 
-    /**
-     * Creates a new ConsoleWriter for a given InputStream.
-     *
-     * @param inputStream the input stream to read from
-     * @param prefix a string to prepend to each line (e.g., "STDOUT: " or "STDERR: ")
-     */
-    public ConsoleWriter(InputStream inputStream, String prefix) {
-        this.inputStream = inputStream;
-        this.prefix = prefix;
-    }
+	/**
+	 * Creates a new ConsoleWriter for a given InputStream.
+	 *
+	 * @param inputStream the input stream to read from
+	 * @param prefix      a string to prepend to each line (e.g., "STDOUT: " or
+	 *                    "STDERR: ")
+	 */
+	public ConsoleWriter(InputStream inputStream, String prefix) {
+		this.inputStream = inputStream;
+		this.prefix = prefix;
+	}
 
-    /**
-     * Continuously reads lines from the input stream and sends them as
-     * ConsoleUpdateDebugEvents to the event queue.
-     */
-    @Override
-    public void run() {
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader reader = new BufferedReader(inputStreamReader);
-        String line;
-
-        try {
-            while (Objects.nonNull(line = reader.readLine())) {
-                String text = prefix + line;
-                ConsoleUpdateDebugEvent consoleUpdateDebugEvent = new ConsoleUpdateDebugEvent(
-                        SimpleDebuggerEventType.REFRESH_CONSOLE,
-                        text
-                );
-                SimpleDebuggerEventQueue.instance().collectDebugEvent(consoleUpdateDebugEvent);
-            }
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-    }
+	/**
+	 * Continuously reads lines from the input stream and sends them as
+	 * ConsoleUpdateDebugEvents to the event queue.
+	 */
+	@Override
+	public void run() {
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+		String line;
+		try {
+			while (Objects.nonNull(line = bufferedReader.readLine())) {
+				String text = prefix + line;
+				SimpleDebuggerEventQueue.instance().collectDebugEvent(new ConsoleUpdateDebugEvent(
+						SimpleDebuggerEventType.REFRESH_CONSOLE, text));
+			}
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+	}
 }
