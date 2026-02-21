f704e393470086e70e59226f1933a19b6354e29e
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-02-21 21:53:04 +0400

    finds representation

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/utils/package-info.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/utils/package-info.java
@@ -6,13 +6,11 @@
  * </p>
  *
  * <p>
- * Author: Sergei Aidinov
- * <br>
+ * Author: Sergei Aidinov <br>
  * Email: <a href="mailto:sergey.aydinov@gmail.com">sergey.aydinov@gmail.com</a>
  * <br>
  * LinkedIn: <a href="https://www.linkedin.com/in/sergei-aydinov/">
- * https://www.linkedin.com/in/sergei-aydinov/
- * </a>
+ * https://www.linkedin.com/in/sergei-aydinov/ </a>
  * </p>
  *
  * @since 1.0
