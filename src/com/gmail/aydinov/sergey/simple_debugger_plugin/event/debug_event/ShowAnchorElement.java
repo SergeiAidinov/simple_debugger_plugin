6b80a746f1e3c1bd3fb6f08d69b175410c7d8e35
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-02-22 21:57:10 +0400

    started disp. anchor

---
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/event/debug_event/ShowAnchorElement.java
@@ -0,0 +1,18 @@
+package com.gmail.aydinov.sergey.simple_debugger_plugin.event.debug_event;
+
+import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.TargetApplicationElementRepresentation;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.event.SimpleDebuggerEventTypes.EventType;
+
+public class ShowAnchorElement extends AbstractSimpleDebugEvent {
+	
+	private final TargetApplicationElementRepresentation anchorElement;
+
+	public ShowAnchorElement(TargetApplicationElementRepresentation anchorElement) {
+		super(EventType.SHOW_ANCHOR_ELEMENT);
+		this.anchorElement = anchorElement;
+	}
+
+	public TargetApplicationElementRepresentation getAnchorElement() {
+		return anchorElement;
+	}
+}
