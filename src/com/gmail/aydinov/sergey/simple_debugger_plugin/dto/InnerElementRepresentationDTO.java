9feb1bdc1b574787dba8f35657a1ddb0ccc3a60b
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-02-28 23:51:58 +0400

    remade DTO

---
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/dto/InnerElementRepresentationDTO.java
@@ -0,0 +1,38 @@
+package com.gmail.aydinov.sergey.simple_debugger_plugin.dto;
+
+import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.InnerElementRepresentation;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.AbstractElementRepresentation.TargetApplicationElementType;
+
+/**
+ * DTO для внутреннего элемента (поле / метод) для передачи в UI.
+ */
+public class InnerElementRepresentationDTO {
+
+    private final String name;
+    private final String typeName;
+    private final TargetApplicationElementType elementType;
+    private final String value;
+
+    public InnerElementRepresentationDTO(InnerElementRepresentation original) {
+        this.name = original.getElementName();
+        this.typeName = original.getFullQualifiedName();
+        this.elementType = original.getElementType();
+        this.value = original.getValue();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public TargetApplicationElementType getElementType() {
+        return elementType;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
