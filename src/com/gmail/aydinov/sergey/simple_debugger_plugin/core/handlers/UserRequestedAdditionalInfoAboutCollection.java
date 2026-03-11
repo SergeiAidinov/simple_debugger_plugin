c06a7731401689db95cb5a373c7554684d5773cd
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-03-11 22:40:49 +0400

    done

---
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/handlers/UserRequestedAdditionalInfoAboutCollection.java
@@ -0,0 +1,22 @@
+package com.gmail.aydinov.sergey.simple_debugger_plugin.core.handlers;
+
+import com.gmail.aydinov.sergey.simple_debugger_plugin.core.interfaces.UIEventHandler;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.InnerElementRepresentationDTO;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.event.SimpleDebuggerEventTypes.SimpleDebuggerEventType;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.event.ui_event.AbstractUIEvent;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.event.ui_event.UIEvent;
+import com.sun.jdi.StackFrame;
+import com.sun.jdi.event.BreakpointEvent;
+
+public class UserRequestedAdditionalInfoAboutCollection implements UIEventHandler{
+
+	@Override
+	public boolean handle(AbstractUIEvent abstractSimpleDebuggerUIEvent, StackFrame currentFrame,
+			BreakpointEvent breakpointEvent) {
+		UIEvent<InnerElementRepresentationDTO> userRequestedAdditionalInfo = (UIEvent<InnerElementRepresentationDTO>) abstractSimpleDebuggerUIEvent;
+		System.out.println(SimpleDebuggerEventType.USER_REQUESTED_ADDITIONAL_INFO_ABOUT_COLLECTION.name()
+				+ userRequestedAdditionalInfo.toString());
+		return false;
+	}
+
+}
