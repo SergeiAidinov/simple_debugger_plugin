a4d31e28762bbf77786eee727f4f86ab6f351c00
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-03-11 20:55:07 +0400

    made UserChangedVariableHandler

---
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/interfaces/UIEventHandler.java
@@ -0,0 +1,16 @@
+package com.gmail.aydinov.sergey.simple_debugger_plugin.core.interfaces;
+
+import com.gmail.aydinov.sergey.simple_debugger_plugin.event.ui_event.UIEvent;
+import com.sun.jdi.StackFrame;
+import com.sun.jdi.event.BreakpointEvent;
+
+@FunctionalInterface
+public interface UIEventHandler<T> {
+    /**
+     * Обрабатывает событие UI.
+     *
+     * @param event событие UI
+     * @return true, если после обработки нужно обновить snapshot и UI
+     */
+    boolean handle(UIEvent<T> uiEvent, StackFrame currentFrame, BreakpointEvent breakpointEvent);
+}
