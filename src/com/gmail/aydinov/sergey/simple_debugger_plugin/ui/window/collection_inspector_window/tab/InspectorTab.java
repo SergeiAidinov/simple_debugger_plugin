8b0700f0c1970087c249f4435a09309d8fe6ebab
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-03-14 11:18:11 +0400

    made tab for arrays

---
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/ui/window/collection_inspector_window/tab/InspectorTab.java
@@ -0,0 +1,12 @@
+package com.gmail.aydinov.sergey.simple_debugger_plugin.ui.window.collection_inspector_window.tab;
+
+import java.util.List;
+
+import org.eclipse.swt.widgets.Composite;
+
+import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.CollectionEntryDTO;
+
+public interface InspectorTab {
+    Composite getControl();
+    void showCollection(List<CollectionEntryDTO> elements);
+}
