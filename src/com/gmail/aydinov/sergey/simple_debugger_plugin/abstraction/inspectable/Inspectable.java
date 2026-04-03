adfde760e32b8bd16aaea383a2cc4b3617b7b305
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-04-03 22:27:26 +0400

    made interfaces

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/Inspectable.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/Inspectable.java
@@ -2,8 +2,18 @@ package com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.inspectable;
 
 import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.inspection.AbstractInspectionDTO;
 
-public interface Inspectable {
-	
-	AbstractInspectionDTO inspect(AbstractInspectableElement inspectableElement);
-
+/**
+ * Represents an inspectable element that can be converted into a DTO representation.
+ *
+ * @param <T> the type of DTO returned by the inspection, must extend AbstractInspectionDTO
+ */
+public interface Inspectable<T extends AbstractInspectionDTO> {
+    
+    /**
+     * Inspect the given element and return a DTO representation of it.
+     *
+     * @param inspectableElement the element to inspect
+     * @return a DTO containing the inspection result
+     */
+    T inspect(AbstractInspectableElement inspectableElement);
 }
--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/PageableInspectable.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/PageableInspectable.java
@@ -1,9 +1,21 @@
 package com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.inspectable;
 
-import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.inspection.AbstractInspectionDTO;
+import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.inspection.AbstractInspectionCollectionPage;
 
-public interface PageableInspectable {
-	
-	AbstractInspectionDTO inspectPage(AbstractInspectableElement inspectableElement, int pageNumber);
+/**
+ * Represents an inspectable collection that supports pagination.
+ *
+ * @param <T> the type of DTO representing a page of the collection,
+ *           must extend AbstractInspectionCollectionPage with any element type
+ */
+public interface PageableInspectable<T extends AbstractInspectionCollectionPage<?>> {
 
+    /**
+     * Retrieve a specific page of the inspected collection.
+     *
+     * @param inspectableElement the collection element to inspect
+     * @param pageNumber zero-based page number to retrieve
+     * @return a DTO representing the requested page of the collection
+     */
+    T inspectPage(AbstractInspectableElement inspectableElement, int pageNumber);
 }
