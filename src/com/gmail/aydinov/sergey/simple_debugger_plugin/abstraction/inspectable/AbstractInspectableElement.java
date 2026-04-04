630c5ad86882dc096481f1e2863730377af70878
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-04-04 15:37:11 +0400

    removed redundant icons

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/AbstractInspectableElement.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/abstraction/inspectable/AbstractInspectableElement.java
@@ -14,48 +14,43 @@ import com.sun.jdi.event.BreakpointEvent;
 
 public abstract class AbstractInspectableElement {
 
-    private final Tag tag;
-    private final String elementName;
-    private final UniversalElementType elementType;
-    private final ValueCategory valueCategory;
-    private final boolean inspectable;
-    private List<BreadcrumbItemDTO> breadcrumbs;
+	private final Tag tag;
+	private final String elementName;
+	private final UniversalElementType elementType;
+	private final ValueCategory valueCategory;
+	private final boolean inspectable;
+	private List<BreadcrumbItemDTO> breadcrumbs;
 
-    public AbstractInspectableElement(
-            Tag tag,
-            String elementName,
-            UniversalElementType elementType,
-            ValueCategory valueCategory,
-            boolean inspectable
-    ) {
-        this.tag = tag;
-        this.elementName = elementName;
-        this.elementType = elementType;
-        this.valueCategory  = valueCategory;
-        this.inspectable = inspectable;
-    }
+	public AbstractInspectableElement(Tag tag, String elementName, UniversalElementType elementType,
+			ValueCategory valueCategory, boolean inspectable) {
+		this.tag = tag;
+		this.elementName = elementName;
+		this.elementType = elementType;
+		this.valueCategory = valueCategory;
+		this.inspectable = inspectable;
+	}
 
-    public Tag getTag() {
-        return tag;
-    }
+	public Tag getTag() {
+		return tag;
+	}
 
-    public String getElementName() {
-        return elementName;
-    }
+	public String getElementName() {
+		return elementName;
+	}
 
-    public UniversalElementType getElementType() {
-        return elementType;
-    }
+	public UniversalElementType getElementType() {
+		return elementType;
+	}
 
-    public boolean isInspectable() {
-        return inspectable;
-    }
-    
-    public ValueCategory getValueCategory() {
+	public boolean isInspectable() {
+		return inspectable;
+	}
+
+	public ValueCategory getValueCategory() {
 		return valueCategory;
 	}
-    
-    public List<BreadcrumbItemDTO> getBreadcrumbs() {
+
+	public List<BreadcrumbItemDTO> getBreadcrumbs() {
 		return breadcrumbs;
 	}
 
@@ -63,39 +58,31 @@ public abstract class AbstractInspectableElement {
 		this.breadcrumbs = breadcrumbs;
 	}
 
-    // ================= Factory =================
+	// ================= Factory =================
 	public static Factory factory() {
-        return new Factory();
-    }
+		return new Factory();
+	}
 
-    public static class Factory {
+	public static class Factory {
 
-        private Factory() {}
+		private Factory() {
+		}
 
-        public AbstractInspectableElement createInspectableElement(
-                AbstractUIEvent event,
-                StackFrame currentFrame,
-                BreakpointEvent breakpointEvent
-        ) {
-            return switch (event.getType()) {
-
-                case USER_STARTED_INSPECTION_SEANCE -> {
-                    if (!(event instanceof UIEvent<?> rawEvent)) {
-                        throw new IllegalArgumentException("Invalid event type: " + event);
-                    }
-
-                    Object payload = rawEvent.getPayload();
-                    if (!(payload instanceof InnerElementRepresentationDTO dto)) {
-                        throw new IllegalArgumentException("Invalid payload: " + payload);
-                    }
-
-                    yield new InspectableIterableElement(dto, currentFrame, breakpointEvent);
-                }
-
-                default -> throw new UnsupportedOperationException(
-                        "Unsupported event: " + event.getType()
-                );
-            };
-        }
-    }
+		public AbstractInspectableElement createInspectableElement(AbstractUIEvent event, StackFrame currentFrame,
+				BreakpointEvent breakpointEvent) {
+			return switch (event.getType()) {
+			case USER_STARTED_INSPECTION_SEANCE -> {
+				if (!(event instanceof UIEvent<?> rawEvent)) {
+					throw new IllegalArgumentException("Invalid event type: " + event);
+				}
+				Object payload = rawEvent.getPayload();
+				if (!(payload instanceof InnerElementRepresentationDTO dto)) {
+					throw new IllegalArgumentException("Invalid payload: " + payload);
+				}
+				yield new InspectableIterableElement(dto, currentFrame, breakpointEvent);
+			}
+			default -> throw new UnsupportedOperationException("Unsupported event: " + event.getType());
+			};
+		}
+	}
 }
