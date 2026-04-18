f0f005e7870debc5c1554a0d6a265603c20f8a8b
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-04-18 06:23:24 +0400

    UserObjectInspectionHandler finds all elements

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/handlers/UserObjectInspectionHandler.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/handlers/UserObjectInspectionHandler.java
@@ -1,36 +1,21 @@
 package com.gmail.aydinov.sergey.simple_debugger_plugin.core.handlers;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.AbstractElementRepresentation;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.AbstractElementRepresentation.Tag;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.UniversalElementRepresentation;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.UniversalElementRepresentation.UniversalElementType;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.abstraction.data_model.TargetApplicationRepresentation;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.core.DebuggerContext;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.core.DebuggerContext.SimpleDebuggerStatus;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.core.interfaces.UIEventHandler;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.InnerElementRepresentationDTO;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.InnerElementRepresentationDTO.InnerElementRepresentationDTOFactory;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.inspection.UserObjectInspectionDTO;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.dto.ui_dto.inspection.UserObjectPageDTO;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.SimpleDebuggerEventTypes.SimpleDebuggerEventType;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.collectors.DebugEventCollector;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.collectors.SimpleDebuggerEventCollector;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.event.collectors.UiEventCollector;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.debug_event.DebugEvent;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.ui_event.AbstractUIEvent;
 import com.gmail.aydinov.sergey.simple_debugger_plugin.event.ui_event.UIEvent;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.ui.window.SimpleDebugerWindowsManager;
-import com.gmail.aydinov.sergey.simple_debugger_plugin.utils.DebugUtils;
 import com.sun.jdi.StackFrame;
 import com.sun.jdi.event.BreakpointEvent;
 
@@ -38,6 +23,7 @@ public class UserObjectInspectionHandler implements UIEventHandler {
 
 	private final DebugEventCollector debugEventCollector = SimpleDebuggerEventCollector.instance();
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public boolean handle(AbstractUIEvent abstractSimpleDebuggerUIEvent, StackFrame currentFrame,
 			BreakpointEvent breakpointEvent) {
@@ -53,47 +39,55 @@ public class UserObjectInspectionHandler implements UIEventHandler {
 		}
 		if (Objects.nonNull(uiEvent)) {
 			InnerElementRepresentationDTO userObject = uiEvent.getPayload();
-			Optional<UniversalElementRepresentation> inspectableObjectOptional = findUserObject(userObject);
-			if (inspectableObjectOptional.isEmpty())
-				return false;
-			List<InnerElementRepresentationDTO> fields =  TargetApplicationRepresentation.getInstance()
-					.getAllElements().stream().filter(e -> e instanceof UniversalElementRepresentation)
-					.map(e -> (UniversalElementRepresentation) e).filter(e -> Objects.equals(e.getTag().getParentId(),
-							inspectableObjectOptional.get().getTag().getUniqueId()))
-					.toList().stream()
-					.map(e -> InnerElementRepresentationDTO.InnerElementRepresentationDTOFactory.fromElement(e))
-					.toList();
-
-			Optional<UniversalElementRepresentation> classElementOptional = TargetApplicationRepresentation
-					.getInstance().getAllElements().stream().filter(e -> e instanceof UniversalElementRepresentation)
-					.map(e -> (UniversalElementRepresentation) e)
-					.filter(e -> Objects.equals(e.getAdditionalInfo(), userObject.getElementName()))
-					.filter(e -> Objects.isNull(e.getTag().getParentId())).findAny();
+			List<UniversalElementRepresentation> uObjs = findAllUserObjects(userObject);
+			List<InnerElementRepresentationDTO> fields = new ArrayList<InnerElementRepresentationDTO>();
 			List<InnerElementRepresentationDTO> methods = new ArrayList<InnerElementRepresentationDTO>();
-			if (classElementOptional.isPresent()) {
+			List<UniversalElementRepresentation> classElementList = new ArrayList<UniversalElementRepresentation>();
+			for (UniversalElementRepresentation universalElementRepresentation : uObjs) {
+				fields.addAll(TargetApplicationRepresentation.getInstance().getAllElements().stream()
+						.filter(e -> e instanceof UniversalElementRepresentation)
+						.map(e -> (UniversalElementRepresentation) e)
+						.filter(e -> Objects.equals(e.getTag().getParentId(),
+								universalElementRepresentation.getTag().getUniqueId()))
+						.toList().stream()
+						.map(e -> InnerElementRepresentationDTO.InnerElementRepresentationDTOFactory.fromElement(e))
+						.toList());
+				classElementList.addAll(TargetApplicationRepresentation.getInstance().getAllElements().stream()
+						.filter(e -> e instanceof UniversalElementRepresentation)
+						.map(e -> (UniversalElementRepresentation) e)
+						.filter(e -> Objects.equals(e.getAdditionalInfo(),
+								universalElementRepresentation.getAdditionalInfo()))
+						.filter(e -> Objects.isNull(e.getTag().getParentId())).toList());
+			}
+			for (UniversalElementRepresentation classRepresentation : classElementList) {
 				methods.addAll(TargetApplicationRepresentation.getInstance().getAllElements().stream()
 						.filter(e -> e instanceof UniversalElementRepresentation)
 						.map(e -> (UniversalElementRepresentation) e)
-						.filter(e -> e.getElementType() == UniversalElementType.METHOD).filter(e -> Objects
-								.equals(e.getTag().getParentId(), classElementOptional.get().getTag().getUniqueId()))
-						.toList().stream().map(e -> InnerElementRepresentationDTO.InnerElementRepresentationDTOFactory.fromElement(e))
+						.filter(e -> e.getElementType() == UniversalElementType.METHOD)
+						.filter(e -> Objects.equals(e.getTag().getParentId(),
+								classRepresentation.getTag().getUniqueId()))
+						.toList().stream()
+						.map(e -> InnerElementRepresentationDTO.InnerElementRepresentationDTOFactory.fromElement(e))
 						.toList());
 			}
 			List<InnerElementRepresentationDTO> subordinates = new ArrayList<InnerElementRepresentationDTO>(fields);
 			subordinates.addAll(methods);
+			subordinates = subordinates.stream().distinct().toList();
 			UserObjectPageDTO userObjectPageDTO = UserObjectPageDTO.builder().elementName(userObject.getElementName())
-					.entries(subordinates).classType(userObject.getTypeOrReturnType()).anchorTag(userObject.getTag()).build();
+					.entries(subordinates).classType(userObject.getTypeOrReturnType()).anchorTag(userObject.getTag())
+					.build();
 			debugEventCollector.collectDebugEvent(new DebugEvent<>(
 					SimpleDebuggerEventType.DISPLAY_PAGE_OF_INSPECTABLE_USER_OBJECT, userObjectPageDTO));
 		}
 		return true;
+
 	}
 
-	private Optional<UniversalElementRepresentation> findUserObject(InnerElementRepresentationDTO anchor) {
+	private List<UniversalElementRepresentation> findAllUserObjects(InnerElementRepresentationDTO anchor) {
 		return TargetApplicationRepresentation.getInstance().getAllElements().stream()
 				.filter(e -> e instanceof UniversalElementRepresentation).map(e -> (UniversalElementRepresentation) e)
 				.filter(e -> Objects.nonNull(e.getObjectReference())).filter(e -> Objects
 						.equals(String.valueOf(e.getObjectReference().uniqueID()), anchor.getAdditionalInfo()))
-				.findFirst();
+				.toList();
 	}
 }
