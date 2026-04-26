78cc1c78ef6a65d1279f6af82295b0fe568a03e8
Sergei Aidinov <sergey.aydinov@gmail.com>
2026-04-26 06:58:03 +0400

    works

--- src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/SimpleDebuggerWorkFlow.java
+++ src/com/gmail/aydinov/sergey/simple_debugger_plugin/core/SimpleDebuggerWorkFlow.java
@@ -131,7 +131,7 @@ public class SimpleDebuggerWorkFlow {
 					TargetApplicationBreakpointRepresentation.getInstance()
 							.onClassPrepared(classPrepareEvent.referenceType());
 				} else if (event instanceof BreakpointEvent) {
-					DebugSession debugSession = new DebugSessionImpl(eventSet);
+					DebugSession debugSession = new DebugSessionImpl(event);
 					Thread sessionThread = new Thread(debugSession);
 					sessionThread.setDaemon(true);
 					sessionThread.start();
@@ -141,7 +141,7 @@ public class SimpleDebuggerWorkFlow {
 					}
 				}
 			}
-		//	eventSet.resume();
+			eventSet.resume();
 		}
 	}
 
